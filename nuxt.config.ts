533202a0b6714bbc6362bda323624adac353a529
TaymaZ <taymazak1382@gmail.com>
2024-03-08 14:44:06 +0330

    feat: Homepage pre-rendered at build time

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,10 @@ export default defineNuxtConfig({
     'nuxt-lucide-icons',
     'nuxt-marquee',
   ],
+  routeRules: {
+    // Homepage pre-rendered at build time
+    '/': { prerender: true },
+  },
   app: {
     head: {
       viewport: 'width=device-width,initial-scale=1',
