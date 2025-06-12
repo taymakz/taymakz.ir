d28a9793e10d2d57e72ed435c8e006cc4c4aceb5
Taymakz <taymazak1382@gmail.com>
2025-06-12 23:29:41 +0330

    feat: enhance theme toggle with view transition animations

--- app/components/ToggleTheme.vue
+++ app/components/ToggleTheme.vue
@@ -16,39 +16,14 @@ const isAppearanceTransition =
 	!window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
 function toggle(event?: MouseEvent) {
-	if (!isAppearanceTransition || !event) {
+	if (!isAppearanceTransition) {
 		isDark.value = !isDark.value;
 		return;
 	}
 
-	const x = event.clientX;
-	const y = event.clientY;
-	const endRadius = Math.hypot(
-		Math.max(x, innerWidth - x),
-		Math.max(y, innerHeight - y),
-	);
-	const transition = document.startViewTransition(async () => {
+	document.startViewTransition(async () => {
 		isDark.value = !isDark.value;
 		await nextTick();
-	});
-
-	transition.ready.then(() => {
-		const clipPath = [
-			`circle(0px at ${x}px ${y}px)`,
-			`circle(${endRadius}px at ${x}px ${y}px)`,
-		];
-		document.documentElement.animate(
-			{
-				clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
-			},
-			{
-				duration: 500,
-				easing: 'ease-in',
-				pseudoElement: isDark.value
-					? '::view-transition-old(root)'
-					: '::view-transition-new(root)',
-			},
-		);
 	});
 }
 </script>
@@ -56,7 +31,7 @@ function toggle(event?: MouseEvent) {
 <template>
 	<button
 		class="text-text/60 hover:text-text/80"
-		@click="toggle"
+		@click="toggle($event)"
 		title="Toggle Theme"
 	>
 		<IconMoon class="dark:hidden" />
