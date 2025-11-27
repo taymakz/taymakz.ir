28ba4de31da46d2949e6f4ea7f527f42c2806baf
taymakz <taymazak1382@gmail.com>
2025-11-27 19:21:34 +0330

    feat: new studenthub logo

--- app/components/Introduce.vue
+++ app/components/Introduce.vue
@@ -31,18 +31,27 @@ const links = [
     <div class="flex items-center gap-2">
       <p class="text-muted-foreground"> Creator of</p>
       <div class="flex items-center gap-1.5 ">
-        <NuxtLink external to="//cafify.ir" target="_blank" rel="noopener noreferrer"
-          aria-label="Cafify – Digital Menu Solution for Cafes and Restaurants by Taymaz Akbari"
-          title="Cafify – Digital Menu Solution for Cafes and Restaurants by Taymaz Akbari"
-          class="bg-card shadow-xs border dark:border-transparent py-0.5 px-2 rounded-md text-primary hover:text-primary/80">
-          <img :src="CafifyLogo" alt="Cafify Logo" class="inline h-6.5 w-auto">
-          Cafify
-        </NuxtLink>
         <NuxtLink external to="//student-hub.ir" target="_blank" rel="noopener noreferrer"
           aria-label="Studenthub – Modern Student Community Web Application by Taymaz Akbari"
           title="Studenthub – Modern Student Community Web Application by Taymaz Akbari"
-          class="bg-card shadow-xs border dark:border-transparent py-0.5 px-2 rounded-md text-primary hover:text-primary/80">
+          class="bg-card hover:bg-accent/50 shadow-xs border dark:border-transparent py-1 px-2 rounded-md flex items-center gap-1.5">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 446.12 508.32"
+              class="h-4.5 text-neutral-900 dark:text-foreground">
+              <path fill="currentColor"
+                d="M439.3,1.2l-213.46,128.62c-1.43.86-3.22.86-4.64,0L6.82.65C3.82-1.15,0,1.01,0,4.51v264.58c0,1.58.83,3.04,2.18,3.85l219.02,131.97c1.43.86,3.22.86,4.64,0l218.1-131.42c1.35-.81,2.18-2.28,2.18-3.85V5.06c0-3.5-3.82-5.66-6.82-3.85Z" />
+              <path fill="currentColor"
+                d="M446.12,315.63v56.77c0,1.58-.83,3.04-2.18,3.85l-218.1,131.42c-1.43.86-3.22.86-4.64,0L2.18,375.7c-1.35-.81-2.18-2.28-2.18-3.85v-56.77c0-3.5,3.82-5.66,6.82-3.85l187.25,112.83h.01l27.12,16.34c1.43.86,3.22.86,4.65,0l213.45-128.62c3-1.81,6.82.35,6.82,3.85Z" />
+            </svg>
+          </span>
           Studenthub
+        </NuxtLink>
+        <NuxtLink external to="//cafify.ir" target="_blank" rel="noopener noreferrer"
+          aria-label="Cafify – Digital Menu Solution for Cafes and Restaurants by Taymaz Akbari"
+          title="Cafify – Digital Menu Solution for Cafes and Restaurants by Taymaz Akbari"
+          class="bg-card hover:bg-accent/50 shadow-xs border dark:border-transparent py-1 px-2 rounded-md flex items-center gap-1.5">
+          <img :src="CafifyLogo" alt="Cafify Logo" class="inline h-6.5 w-auto">
+          Cafify
         </NuxtLink>
 
         <span class="sr-only">Cafify is a digital menu solution for cafes and restaurants.</span>
